c4e29be395240267f2942475fe3c6566faa2fa05
livolickie <havingteam@gmail.com>
2022-11-17 17:56:12 +0500

    добавлена генерация данных, базовое отображение клиентов на карте, 3 тренда

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,8 +5,12 @@
         <Nuxt />
       </v-container>
     </v-main>
-    <v-footer :absolute="!fixed" app>
-      <span>&copy; PI - {{ new Date().getFullYear() }}</span>
+    <v-footer :absolute="!fixed" app class="d-flex justify-space-between">
+      <span>&copy; ПИ - {{ new Date().getFullYear() }}</span>
+      <div class="d-flex flex-row align-center">
+        <v-switch v-model="$vuetify.theme.dark" ></v-switch>
+        <span>Время: {{ time }}</span>
+      </div>
     </v-footer>
   </v-app>
 </template>
@@ -16,26 +20,18 @@ export default {
   name: 'DefaultLayout',
   data() {
     return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/',
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire',
-        },
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js',
+      time: ''
     }
   },
+  methods: {
+    updateTime() {
+      const date = new Date()
+      this.time = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+    }
+  },
+  mounted() {
+    this.updateTime()
+    setInterval(this.updateTime, 1000)
+  }
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container fill-height>
-    <v-row>
+    <v-row v-if="status != 'reload'">
       <v-col lg="2">
         <v-list dense>
           <v-subheader>Показатели</v-subheader>
@@ -28,7 +28,7 @@
                 <v-icon>mdi-account-multiple</v-icon>
               </v-list-item-icon>
               <v-list-item-content>
-                Клиентов: {{ clients.length }}
+                Клиентов: {{ activeClients.length }}
               </v-list-item-content>
             </v-list-item>
 
@@ -43,11 +43,13 @@
           </v-list-item-group>
         </v-list>
 
-        <v-card class="pa-2 mt-4">
+        <v-card class="pa-2 mt-4 d-flex flex-column">
           <h3 class="text-center mb-2">Система: <span class="pa-1 success">OK</span></h3>
-          <v-btn class="mb-2" color="">Перезагрузить</v-btn>
-          <v-btn class="mb-2" color="">Отключить</v-btn>
-          <v-btn class="mb-2" color="">Обновить</v-btn>
+          <v-btn class="mb-2" @click="status = 'reload'; reload()">Перезагрузить</v-btn>
+          <v-btn v-if="status != 'reload'" class="mb-2" :color="status != 'pause' ? '' : 'success'"
+            @click="status == 'active' ? status = 'pause' : status = 'active'; snack = status == 'pause'; snackMessage='Система поставлена на паузу'">{{ status != 'active' ? 'Возобновить' :
+                'Пауза'
+            }}</v-btn>
         </v-card>
       </v-col>
 
@@ -59,11 +61,12 @@
             </v-scroll-x-transition>
             <v-container>
               <v-row>
-                <v-col lg="1">Лимит ({{maxPower}}):</v-col>
+                <v-col lg="1">Лимит ({{ maxPower }}):</v-col>
                 <v-col>
-                  <v-slider v-model="maxPower" step="10" thumb-label ticks></v-slider>
+                  <v-slider v-model="maxPower" min="90" max="180" step="10" thumb-label ticks></v-slider>
                 </v-col>
               </v-row>
+              <h3>Затрачено: {{spentPower}} dbW</h3>
             </v-container>
           </template>
           <template v-if="tab == 1">
@@ -72,11 +75,12 @@
             </v-scroll-x-transition>
             <v-container>
               <v-row>
-              <v-col lg="1">Лимит ({{maxEnergy}}):</v-col>
-              <v-col>
-                <v-slider v-model="maxEnergy" step="10" thumb-label ticks></v-slider>
-              </v-col>
-            </v-row>
+                <v-col lg="1">Лимит ({{ maxEnergy }}):</v-col>
+                <v-col>
+                  <v-slider v-model="maxEnergy" min="350" max="800" step="10" thumb-label ticks></v-slider>
+                </v-col>
+              </v-row>
+              <h3>Затрачено: {{spentEnergy}}W</h3>
             </v-container>
           </template>
           <template v-if="tab == 2">
@@ -96,6 +100,30 @@
       </v-col>
 
     </v-row>
+    <v-row v-else justify="center">
+
+      <v-progress-circular :rotate="360" :size="300" :width="15" :value="load" color="teal" class="text-center"
+        style="margin-top: 15%">
+        <div class="d-flex flex-column">
+          <span>
+            Идёт перезагрузка
+          </span>
+          <span class="fw-bold">
+            {{ load }}%
+          </span>
+        </div>
+      </v-progress-circular>
+    </v-row>
+
+    <canvas width="500" height="400" id="canvas" class="mt-5"></canvas>
+
+    <v-snackbar
+      v-model="snack"
+      :multi-line="multiLine"
+    >
+      {{ snackMessage }}
+    </v-snackbar>
+
   </v-container>
 </template>
 
@@ -105,25 +133,22 @@ export default {
   name: 'IndexPage',
   data() {
     return {
+      status: 'active',
       power: 0,
       energy: 0,
       clients: [
-        {
-          number: '8-800-555-35-35',
-          speed: 12.5
-        },
-        {
-          number: '8-900-555-35-35',
-          speed: 15.5
-        },
-        {
-          number: '8-700-555-35-35',
-          speed: 9.5
-        }
       ],
+      load: 0,
+      loadInterval: null,
 
-      maxPower: 10,
-      maxEnergy: 10,
+      spentPower: 0,
+      spentEnergy: 0,
+      
+      maxPower: 120,
+      maxEnergy: 420,
+
+      snackMessage: '',
+      snack: false,
 
       network: 0,
       tab: 0,
@@ -132,14 +157,27 @@ export default {
         { text: 'Номер', value: 'number' },
         { text: 'Скорость', value: 'speed' },
       ],
+      ctx: null,
       chartData: {
         labels: [],
         datasets: [
           {
             label: 'Сеть',
             data: [],
-            fill: false,
+            fill: true,
             borderColor: 'rgb(75, 192, 192)'
+          },
+          {
+            label: 'Мощность',
+            data: [],
+            fill: true,
+            borderColor: 'rgb(155, 48, 217)'
+          },
+          {
+            label: 'Электроэнергия',
+            data: [],
+            fill: true,
+            borderColor: 'rgb(77, 219, 37)'
           }
         ]
       },
@@ -147,14 +185,20 @@ export default {
     }
   },
   computed: {
+    activeClients() {
+      const clients = this.clients.filter(client => client.active)
+      return clients;
+    },
     tClients() {
       const clients = []
       let id = 0
-      for (const client of this.clients) {
+      for (const client of this.activeClients) {
         clients.push({
           id,
           number: client.number,
-          speed: client.speed
+          speed: client.speed,
+          x: client.x,
+          y: client.y
         })
         id++
       }
@@ -162,19 +206,139 @@ export default {
     }
   },
   mounted() {
-    setInterval(this.update, 1000)
+    this.init()
+    setInterval(this.update, 500)
   },
   methods: {
+    canvas() {
+      const canvas = document.querySelector('#canvas')
+      this.ctx = canvas.getContext('2d')
+      this.ctx.fillStyle = 'rgb(100, 100, 100)'
+      this.ctx.fillRect(0, 0, 800, 600)
+    },
+    init() {
+      this.chartData = {
+        labels: [],
+        datasets: [
+          {
+            label: 'Сеть',
+            data: [],
+            fill: true,
+            borderColor: 'rgb(75, 192, 192)'
+          },
+          {
+            label: 'Мощность',
+            data: [],
+            fill: true,
+            borderColor: 'rgb(155, 48, 217)'
+          },
+          {
+            label: 'Электроэнергия',
+            data: [],
+            fill: true,
+            borderColor: 'rgb(77, 219, 37)'
+          }
+        ]
+      }
+      this.clients = []
+      for (let i = 0; i < 2000; i++) {
+        this.clients.push({
+          number: `8-700-${Math.round(Math.random() * 200 + 300)}-${Math.round(Math.random() * 55 + 20)}-${Math.round(Math.random() * 35 + 10)}`,
+          speed: parseFloat((Math.random() * 10 + 5).toFixed(1)),
+          active: Math.random() > 0.573,
+          x: Math.random() * 500,
+          y: Math.random() * 400
+        })
+
+        this.status = 'active'
+      }
+      this.canvas()
+    },
+    reload() {
+      clearInterval(this.loadInterval)
+      this.load = 0
+      this.loadInterval = setInterval(() => {
+        if (this.reload) {
+          this.load += 14
+        } else {
+          clearInterval(this.loadInterval)
+        }
+      }, 300)
+      setTimeout(this.init, 3000)
+    },
     selectTab(_tab) {
       this.tab = _tab
     },
     update() {
-      this.power++
-      this.energy++
-      this.network++
 
+      if (this.status !== 'active') return
+
+      // Генерация рандомных данных (отлючение - подключение клиентов)
+      this.ctx.fillStyle = 'rgb(100, 100, 100)'
+      this.ctx.fillRect(0, 0, 800, 600)
+      this.ctx.fillStyle = 'rgb(0, 100, 0)'
+      for (const client of this.clients) {
+        if (client.active && Math.random() > 0.99) {
+          client.active = false
+        }
+        else if (!client.active && Math.random() < 0.01) client.active = true
+
+        if (client.active) {
+          const rnd = Math.random()
+          if (rnd > 0.98) {
+            const changeVal = Math.random() * 1 + 0.5
+            if (client.speed < 15) client.speed += changeVal
+            else client.speed -= changeVal
+
+            client.speed = parseFloat(client.speed.toFixed(1))
+          }
+
+
+          if (Math.random() < 0.25) {
+            if (client.x < 500 && Math.random() > 0.5) client.x += Math.random() * 5
+            else client.x -= Math.random() * 5
+          }
+          else if (Math.random() > 0.75) {
+            if (client.y < 500 && Math.random() < 0.5) client.y += Math.random() * 5
+            else client.y -= Math.random() * 5
+          }
+
+          // Canvas
+          this.ctx.beginPath()
+          this.ctx.arc(client.x, client.y, 2, 0, 2 *Math.PI)
+          this.ctx.stroke()
+        }
+      }
+
+      this.network = 0
+      // Сеть будет зависеть от клиентов
+      for (const client of this.activeClients) {
+        this.network += client.speed / 10
+      }
+      this.network = Math.round(this.network)
+
+
+      this.spentPower += this.power
+      this.spentEnergy += this.energy
+      this.spentPower = parseFloat(this.spentPower.toFixed(1))
+      this.spendEnergy = parseFloat(this.spentEnergy.toFixed(1))
+      // Мощность зависит от сети
+      this.power = Math.round((this.network / 10 + Math.random()) + 5 * Math.random())
+
+      this.energy = parseFloat((this.power * 3.5 + 3 * Math.random()).toFixed(1))
+
+      if (this.chartData.datasets[0].data.length > 400) {
+        this.chartData.datasets[0].data.shift()
+        this.chartData.datasets[1].data.shift()
+        this.chartData.datasets[2].data.shift()
+      }
+      if (this.chartData.labels.length > 600) {
+        this.chartData.labels.shift()
+      }
       this.chartData.labels.push(new Date().toLocaleTimeString())
       this.chartData.datasets[0].data.push(this.network)
+      this.chartData.datasets[1].data.push(this.power)
+      this.chartData.datasets[2].data.push(this.energy)
     }
   },
 }
